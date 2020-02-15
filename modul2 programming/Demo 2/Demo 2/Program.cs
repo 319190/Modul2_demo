6bf9089477cfe9754c143192fe343d419c48e057
orlin <orlin221@abv.bg>
2020-02-15 11:43:01 +0200

    dfghj

--- modul2 programming/Demo 2/Demo 2/Program.cs
+++ modul2 programming/Demo 2/Demo 2/Program.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo1
+namespace Demo_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Hub");
+            int v = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(v);
+            }
         }
     }
 }
